0eb0f2255a7308e82905a393e18b9960606bcc50
Efe Okan <efe-okan2004@hotmail.com>
2024-02-15 20:32:04 +0100

    01-04 lists tem 05 median done

--- 01-basic-python/04-lists/05-median/student.py
+++ 01-basic-python/04-lists/05-median/student.py
@@ -1,1 +1,29 @@
 # Write your code here
+
+def median(ns):
+    sortedList = sorted(ns)
+    if len(sortedList) == 1:
+        return sortedList[0]
+    
+    elif len(sortedList) == 2:
+        return (sortedList[0] + sortedList[1]) / 2 
+    
+    elif len(sortedList) % 2 == 0:
+        indexLeft = (len(sortedList) // 2) - 1
+        indexRight = indexLeft + 1
+
+        median = (sortedList[indexLeft] + sortedList[indexRight]) / 2
+        return median
+    
+    median = sortedList[len(sortedList) // 2]
+    return median
+
+#oplossing
+# def median(ns):
+#     sorted_ns = sorted(ns)
+#     i = len(sorted_ns) // 2
+
+#     if len(sorted_ns) % 2 == 0:
+#         return (sorted_ns[i - 1] + sorted_ns[i]) / 2
+#     else:
+#         return sorted_ns[i]
