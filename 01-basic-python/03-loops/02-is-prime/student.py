94162cc473e2619f4e910303a19a07f8da850b8c
Efe Okan <efe-okan2004@hotmail.com>
2024-02-15 19:45:40 +0100

    03 loops done

--- 01-basic-python/03-loops/02-is-prime/student.py
+++ 01-basic-python/03-loops/02-is-prime/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+
+#2 is the only even prime number
+#range is exclusive of the last number
+def is_prime(n):
+    if n < 2: 
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+            #if n % i == 0, it means that n is divisible by a different number than itself or 1, so it is not prime
+    return True
