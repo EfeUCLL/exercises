6eb8f041501fc83afd7a275134e568a154f4109b
Efe Okan <efe-okan2004@hotmail.com>
2024-02-19 17:08:48 +0100

    01-04 lists finished

--- 01-basic-python/04-lists/07-target-sum/student.py
+++ 01-basic-python/04-lists/07-target-sum/student.py
@@ -1,1 +1,16 @@
 # Write your code here
+
+def target_sum(ns, target):
+    for i in range(len(ns)):
+        for j in range(i, len(ns)):
+            if ns[i] + ns[j] == target:
+                return True
+    return False
+
+#oplossing
+# def target_sum(ns, target):
+#     for x in ns:
+#         for y in ns:
+#             if x + y == target:
+#                 return True
+#     return False
--- 01-basic-python/04-lists/13-rotate/student.py
+++ 01-basic-python/04-lists/13-rotate/student.py
@@ -1,1 +1,15 @@
 # Write your code here
+from copy import copy
+
+def rotate(xs, n):
+    xs2 = copy(xs)
+    return xs[n:] + xs2[:n]
+
+print(rotate([1, 2, 3], 2))
+
+
+#oplossing
+# def rotate(xs, n):
+#     for _ in range(n):
+#         x = xs.pop(0)
+#         xs.append(x)
