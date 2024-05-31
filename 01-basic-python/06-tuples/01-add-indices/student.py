66382d3fb5af3196005a7d173aaa2ec881e51d98
Efe Okan <efe-okan2004@hotmail.com>
2024-05-31 19:42:52 +0200

    01-basic-python--06-tuples Done

--- 01-basic-python/06-tuples/01-add-indices/student.py
+++ 01-basic-python/06-tuples/01-add-indices/student.py
@@ -1,1 +1,17 @@
 # Write your code here
+
+def add_indices(xs):
+    index_list = []
+    for i in range(len(xs)):
+        index_list.append(i)
+
+    return list(zip(index_list, xs))
+
+# solution:
+# def add_indices(xs):
+#     indices = range(len(xs))
+#     return list(zip(indices, xs))
+#
+# # Better: relying on existing functionality
+# def add_indices(xs):
+#     return list(enumerate(xs))
--- 01-basic-python/06-tuples/02-is-increasing/student.py
+++ 01-basic-python/06-tuples/02-is-increasing/student.py
@@ -1,1 +1,24 @@
 # Write your code here
+
+def is_increasing(ns):
+    ms = ns[1:]
+    paired = list(zip(ns, ms))
+
+    if len(ns) == 0 or len(ns) == 1:
+        return True
+
+    i = len(paired)
+    while i > 0:
+        for j in range(len(paired)):
+            if paired[j][0] <= paired[j][1]:
+                i -= 1
+            else:
+                return False
+        return True
+
+# oplossing:
+# def is_increasing(ns):
+#     for (x, y) in zip(ns, ns[1:]):
+#         if x > y:
+#             return False
+#     return True
